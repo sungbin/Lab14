4a390bc2c70031c9a91d279fa1b3e1466334b8f8
sungbin <21700613@handong.edu>
2018-05-10 00:48:09 +0900

    add single line comment prob6

--- src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
@@ -1,45 +1,45 @@
-package edu.handong.csee.java.lab14.prob6;
+package edu.handong.csee.java.lab14.prob6; //the file is in this package
 
-public class CustomID extends Exception {
-	private String name="";
-	private int age;
-	private String race="";
-	private int state = 0;
-	String str;
+public class CustomID extends Exception { //declare public class, child of Exception
+	private String name=""; //declare type private String variable
+	private int age; //declare private integer type variable
+	private String race=""; //declare private String type variable
+	private int state = 0; //declare private integer type variable
+	String str; //declare String type variable
 
 
-	public void setName(String name) throws Exception {
-		if(name.length() <5)
-			throw new Exception("Your name is short! ");
-		System.out.println("Name is valid");
-		this.name = name;
-		System.out.println("Name: " +this.name);
-		state =1;
+	public void setName(String name) throws Exception { //declare public type, return nothing, parameter type is String, throws Exception type Exception
+		if(name.length() <5) //if name.length() is smaller than 5
+			throw new Exception("Your name is short! "); //make new Exception named that, and throw
+		System.out.println("Name is valid"); //print sentence
+		this.name = name; //put local name value to member name variable
+		System.out.println("Name: " +this.name); //print sentence
+		state =1; //put 1 to state
 	}
 
-	public void setAge(int age) throws Exception {
-		if(age <18)
-			throw new Exception("You are too young! ");
-		System.out.println("Age is valid");
-		this.age = age;
-		System.out.println("Age: " +this.age);
-		state = 2;
+	public void setAge(int age) throws Exception { //declare public type, return nothing, parameter type is integer, throws Exception type Exception
+		if(age <18) //if age is smaller than 18
+			throw new Exception("You are too young! "); //make new Exception named that
+		System.out.println("Age is valid"); //print sentence
+		this.age = age; //put local age value to member age variable
+		System.out.println("Age: " +this.age); //print sentence
+		state = 2; //put 2 to state
 	}
 
-	public void setRace(String race) throws Exception {
-		if(race.equals("Vulcans")||race.equals("Romulan")||race.equals("Klingons"))
+	public void setRace(String race) throws Exception { //make public type, return nothing, parameter type is String, throws Exception type exception
+		if(race.equals("Vulcans")||race.equals("Romulan")||race.equals("Klingons")) //if race equals Vulcans or Romulan, or Klingons
 		{
-			System.out.println("Race is valid");
-			this.race = race;
-			System.out.println("Race: " +this.race);
-			state =0;
-			return;
+			System.out.println("Race is valid"); //print sentence
+			this.race = race; //put local race value to member race variable
+			System.out.println("Race: " +this.race); //print sentence
+			state =0; //put zero to state
+			return; //exit this method
 		}
-		throw new Exception("Human! ");
+		throw new Exception("Human! "); //make new Exception named that
 	}
 
-	public int getState() {
-		return state;
+	public int getState() { //make public type method, return type is integer
+		return state; //return state value
 	}
 	
 	
--- src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
@@ -1,39 +1,39 @@
-package edu.handong.csee.java.lab14.prob6;
+package edu.handong.csee.java.lab14.prob6; //the file is in this package
 
-import java.util.Scanner;
-public class Main {
+import java.util.Scanner; //include Scanner class
+public class Main { //declare public type class
 	
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		CustomID obj = new CustomID();
-		String name, race;
-		int age;
-		boolean repeat= true;
+	public static void main(String[] args) { //declare main method
+		Scanner in = new Scanner(System.in); //make instance of Scanner
+		CustomID obj = new CustomID(); //make instance of CustomID
+		String name, race; //declare String type two variables
+		int age; //declare integer type variable
+		boolean repeat= true; //declare boolean type variable, put true to that
 		
-		while(repeat)
+		while(repeat) //if repaet equals true
 		{
-			try {
-				if(obj.getState() == 0) {
-				System.out.print("Enter your name: ");
-				name = in.nextLine();
-				obj.setName(name);
+			try {//are there any exception?
+				if(obj.getState() == 0) { //if state value equals 0
+				System.out.print("Enter your name: "); //print sentence
+				name = in.nextLine(); //scan one line, and put them to name
+				obj.setName(name); //put name to obj's name
 				}
-			else if(obj.getState() == 1) {
-				System.out.print("Enter your age: ");
-				age = in.nextInt();
-				in.nextLine();
-				obj.setAge(age);
+			else if(obj.getState() == 1) { //if state value equals 1
+				System.out.print("Enter your age: "); //print sentence
+				age = in.nextInt(); //scan one line ,and put them to name
+				in.nextLine(); //scan one line
+				obj.setAge(age); //put age to obj's age
 				}
-			else if(obj.getState() == 2) {
-				System.out.print("Enter your race: ");
-				race = in.nextLine();
-				obj.setRace(race);
+			else if(obj.getState() == 2) { //if state value equals 2
+				System.out.print("Enter your race: "); //print sentence
+				race = in.nextLine(); //scan one line, and put them to race
+				obj.setRace(race);// put race to obj's race
 				}
 			}
-			catch(Exception e){
-				System.out.println(e.getMessage()+"Try again!");
-				continue;
+			catch(Exception e){ //is there Exception type exception?
+				System.out.println(e.getMessage()+"Try again!"); //print sentence
+				continue; //one skip, and keep going roof
 			}
 		}
 	}
