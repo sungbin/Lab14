00a1f04a0f08a326c74c64f88e76f74c161e5410
sungbin <21700613@handong.edu>
2018-05-09 20:12:53 +0900

    add single line comment prob2

--- src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
@@ -1,24 +1,24 @@
-package edu.handong.csee.java.lab14.prob2;
+package edu.handong.csee.java.lab14.prob2; //the file is in this package
 
-import java.util.Scanner;
-public class In_exception {
-	Scanner key = new Scanner(System.in);
-	private int x;
-	private int y;
+import java.util.Scanner; //include Scanner class
+public class In_exception { //declare public type class
+	Scanner key = new Scanner(System.in); //make instance of Scanner
+	private int x; //declare private integer type
+	private int y; //declare private integer type
 	
-	public In_exception(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public In_exception(int x, int y) { //make constructor, parameter type are int, int
+		this.x = x; //put x to member variable x
+		this.y = y; //put y to member variable y
 	}
-	public In_exception() {
-		System.out.print("x: ");
-		this.x = key.nextInt();
-		System.out.print("y: ");
-		this.y = key.nextInt();
+	public In_exception() { //make constructor
+		System.out.print("x: "); //print sentence
+		this.x = key.nextInt(); //scan one integer
+		System.out.print("y: "); //print sentence
+		this.y = key.nextInt(); //scan on integer
 	}
-	public int devideXasY() throws ArithmeticException
+	public int devideXasY() throws ArithmeticException //declare public type, return type is integer
 	{
-		return this.x / this.y;
+		return this.x / this.y; //return value of x/y
 	}
 
 
--- src/main/java/edu/handong/csee/java/lab14/prob2/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/Main.java
@@ -1,20 +1,20 @@
-package edu.handong.csee.java.lab14.prob2;
+package edu.handong.csee.java.lab14.prob2; //the file is in this package
 
-import java.util.InputMismatchException;
+import java.util.InputMismatchException; //include InputMismatchException class
 
-public class Main {
+public class Main { //declare Main class
 
-	public static void main(String[] args) {
-		try {
-			In_exception ie = new In_exception();
-			System.out.println(ie.devideXasY());
-		}catch(InputMismatchException e)
+	public static void main(String[] args) { //declare main method
+		try { //is there any exception?
+			In_exception ie = new In_exception();// make instance of In_exception
+			System.out.println(ie.devideXasY()); //print sentence
+		}catch(InputMismatchException e) //if InputMismatchException type Exception appear
 		{
-			System.out.println(e.toString());
+			System.out.println(e.toString()); //print sentence
 		}
-		catch(ArithmeticException e)
+		catch(ArithmeticException e) //if ArithmeticException type Exception appear
 		{
-			System.out.println(e.toString());
+			System.out.println(e.toString()); //print sentence
 		}
 
 	}
