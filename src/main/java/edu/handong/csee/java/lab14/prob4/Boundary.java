6d1ff8fb12880b9ccf9532edadd5396c332e313a
sungbin <21700613@handong.edu>
2018-05-10 00:31:11 +0900

    add single line comment prob4

--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -1,18 +1,18 @@
-package edu.handong.csee.java.lab14.prob4;
+package edu.handong.csee.java.lab14.prob4; //the file is in this package
 
-import java.util.Scanner;
-public class Boundary {
-	private static int []array = new int [5];
-	private static int count = 0;
-	Scanner in = new Scanner(System.in);
-	public void putArrayIndex() throws ArrayIndexOutOfBoundsException
+import java.util.Scanner; //include Scanner class
+public class Boundary { //declare public type class
+	private static int []array = new int [5]; //declare private static integer array variable
+	private static int count = 0; //declare private static integer variable
+	Scanner in = new Scanner(System.in); //make instance of scanner
+	public void putArrayIndex() throws ArrayIndexOutOfBoundsException //declare public type, return nothing
 	{
-		int newint;
-		System.out.print("Enter an integer: ");
-		newint = in.nextInt();
-		array[count]=newint;
-		System.out.println("arr["+count+"] <- "+newint);
-		count ++;
+		int newint; //declare integer type variable
+		System.out.print("Enter an integer: "); //print sentence
+		newint = in.nextInt(); //scan one integer
+		array[count]=newint; //put newint to array[count]
+		System.out.println("arr["+count+"] <- "+newint); //print sentence
+		count ++; //count plus 1
 	}
 
 }
--- src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
@@ -1,16 +1,16 @@
-package edu.handong.csee.java.lab14.prob4;
+package edu.handong.csee.java.lab14.prob4; //the file is in this package
 
-public class Main {
+public class Main { //declare public type class
 
-	public static void main(String[] args) {
-		Boundary bd = new Boundary();
-		while(true)
+	public static void main(String[] args) { //declare public type, main method
+		Boundary bd = new Boundary(); //make instance of boundary class
+		while(true) //roof infinity
 		{
-			try {
-				bd.putArrayIndex();
-			} catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("Invalid array index access!");
-				break;
+			try { //if there are any exception?
+				bd.putArrayIndex(); //execute putArrayIndex
+			} catch (ArrayIndexOutOfBoundsException e) { //if there is ArrayIndexOutOfBoundsException,
+				System.out.println("Invalid array index access!"); //print sentence
+				break; //escape roof
 			}
 			
 		}
