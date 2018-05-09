0a98a194bfd1f788c88bb9bfa14d03fe8c1f4797
sungbin <21700613@handong.edu>
2018-05-09 20:22:13 +0900

    add single line comment prob3

--- src/main/java/edu/handong/csee/java/lab14/prob3/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Main.java
@@ -1,20 +1,20 @@
-package edu.handong.csee.java.lab14.prob3;
+package edu.handong.csee.java.lab14.prob3; //the file is in this package
 
-import java.util.Scanner;
-public class Main {
+import java.util.Scanner; //include Scanner class
+public class Main { //declare Main class
 
-	public static void main(String[] args) {
-		Powercalc my_calculator = new Powercalc();
-	    Scanner key = new Scanner(System.in);
-		int n,p;
-		while(key.hasNext())
+	public static void main(String[] args) { //declare main method
+		Powercalc my_calculator = new Powercalc(); //make instance Powercalc
+	    Scanner key = new Scanner(System.in); //make instance of Scanner
+		int n,p; //declare integer type two variable
+		while(true) //infinity roop
 		{
-			n = key.nextInt();
-			p = key.nextInt();
-			try {
-				System.out.println(my_calculator.power(n, p));
-			} catch(Exception e) {
-				System.out.println(e);
+			n = key.nextInt(); //scan one integer
+			p = key.nextInt(); //scan one integer
+			try { //is there any exception?
+				System.out.println(my_calculator.power(n, p)); //print sentence
+			} catch(Exception e) { //if there is exception
+				System.out.println(e); //print sentence of e's toString
 			}
 		}
 	}
--- src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
@@ -1,16 +1,16 @@
-package edu.handong.csee.java.lab14.prob3;
+package edu.handong.csee.java.lab14.prob3; //the file is in this package
 
-public class Powercalc {
-	public long power(int n, int p) throws Exception
+public class Powercalc { //declare public type class
+	public long power(int n, int p) throws Exception //declare public type method, return type is long, parameter type is int, int. if there is Exception type Exception, throws them
 	{
-		int m=1;
-		if(n ==0&& p == 0)
-			throw new Exception("n and p should not be zero");
-		if(n<0 || p<0)
-			throw new Exception("n or p should not be negative");
-		for(int i =0; i<p; i++)
-			m*=n;
-		return m;
+		int m=1; //declare local variable, integer type
+		if(n ==0&& p == 0) //if n equal zero and p equal zero
+			throw new Exception("n and p should not be zero"); //make new Exception type Exception, named that
+		if(n<0 || p<0) //if n is smaller than zero and p is smaller then zero
+			throw new Exception("n or p should not be negative");//make new Exception type Exception, named that
+		for(int i =0; i<p; i++) //roof while i is smaller than p
+			m*=n; //put n*m value to m
+		return m; //return m value
 	}
 
 }
